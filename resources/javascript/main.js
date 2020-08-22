f7a6f23e4a77f7a6d8f3f4d292a7114cb2916b79
Ava Lee <ava@Avas-MacBook-Pro.local>
2020-08-21 19:40:34 -0700

    Edited application page and added javascript

---
+++ resources/javascript/main.js
@@ -0,0 +1,21 @@
+// Mobile Nav
+function openNav() {
+  document.getElementById("mobile").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("mobile").style.width = "0%";
+  document.getElementById("mobileShop").style.width = "0%";
+}
+
+// Shop nav
+
+function openShop() {
+  document.getElementById("mobileShop").style.width = "87%";
+}
+
+function closeShop() {
+  document.getElementById("mobileShop").style.width = "0%";
+}
+
+//
