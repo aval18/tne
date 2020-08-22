70277579761ed4625c51ffb9352b6d3e6bae0bda
Ava Lee <ava@Avas-MacBook-Pro.local>
2020-08-22 11:09:01 -0700

    Added green background colour to all other pages except home, changed footer border colour

---
+++ resources/php/contact.php
@@ -0,0 +1,11 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You! We will get back to you shortly :)" . " -" . "<a href='../../contact.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+?>
